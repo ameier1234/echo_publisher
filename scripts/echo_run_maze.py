93c8307caa4744534f7196721a7acc85f0b6bf5d
Alex Meier <Alex_Meier@student.uml.edu>
2017-06-12 14:14:47 -0400

    implemented with classes

--- scripts/echo_run_maze.py
+++ scripts/echo_run_maze.py
@@ -6,17 +6,18 @@ from flask import Flask, render_template
 from flask_ask import Ask, statement
 
 #updated to remove global variables
+app = Flask(__name__)
+ask = Ask(app, '/')
 
 class Intents(object):
-    def __init__():
-        startRacePub = rospy.Publisher("maze", Bool, queue_size=1000)
-        app = Flask(__name__)
-        ask = Ask(app, '/')
+    startRacePub = rospy.Publisher("maze", Bool, queue_size=1000)
+        
         
     def startServer(self, debug):
         self.app.run(debug)
     
-    #gets called when a RunRace intent is recieved from alexa 
+    #gets called when a RunRace intent is recieved from alexa
+    global ask 
     @ask.intent('RunRace')
     def getState(state):
         msg = Bool() 
@@ -25,10 +26,10 @@ class Intents(object):
         else:
             msg.data = False
             
-            response = render_template("mazeResponse", state = state)
+        response = render_template("mazeResponse", state = state)
 
         try:
-            startRacePub.publish(msg)
+            Intents.startRacePub.publish(msg)
         except rospy.ROSInterruptException:
             pass
             
@@ -36,7 +37,8 @@ class Intents(object):
 
 def setup():
     rospy.init_node('StringPublisher', anonymous=True)
-    intents = Intents(debug=True)
+    intents = Intents()
+    app.run(debug=True)
 
 if __name__ == "__main__":
     setup()
